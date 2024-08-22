66d641ffa1186661b8944a33166790e4cd341fde
albayona <al.bayona@uniandes.edu.co>
2024-08-22 02:37:33 -0500

    NA

--- fbbotdashboard/src/components/GroupTable.jsx
+++ fbbotdashboard/src/components/GroupTable.jsx
@@ -7,11 +7,15 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import {Chip, emphasize, TablePagination} from "@mui/material";
+import {Chip, emphasize} from "@mui/material";
 import {styled} from '@mui/material/styles';
 import TableCell from '@mui/material/TableCell';
-import {CollapsibleRow} from "./CollapsibleRow";
+import {Row} from "./Row";
 import NumbersIcon from '@mui/icons-material/Numbers';
+import MuiAccordion from '@mui/material/Accordion';
+import MuiAccordionSummary from '@mui/material/AccordionSummary';
+import MuiAccordionDetails from '@mui/material/AccordionDetails';
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
 
 const StyledTableCell2 = styled(TableCell)(({theme}) => ({
     backgroundColor: theme.palette.primary.main, color: theme.palette.common.white, fontSize: 24,
@@ -51,6 +55,43 @@ export const CustomChip2 = styled(Chip)(({theme}) => {
     };
 });
 
+
+const Accordion = styled((props) => (
+    <MuiAccordion disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+    border: `1px solid ${theme.palette.divider}`,
+    '&:not(:last-child)': {
+        borderBottom: 0,
+    },
+    '&::before': {
+        display: 'none',
+    },
+}));
+
+const AccordionSummary = styled((props) => (
+    <MuiAccordionSummary
+        expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
+        {...props}
+    />
+))(({ theme }) => ({
+    backgroundColor:
+        theme.palette.mode === 'dark'
+            ? 'rgba(255, 255, 255, .05)'
+            : 'rgba(0, 0, 0, .03)',
+    flexDirection: 'row-reverse',
+    '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
+        transform: 'rotate(90deg)',
+    },
+    '& .MuiAccordionSummary-content': {
+        marginLeft: theme.spacing(1),
+    },
+}));
+
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+    padding: theme.spacing(2),
+    borderTop: '1px solid rgba(0, 0, 0, .125)',
+}));
+
 function areGroupTablePropsEqual(oldProps, newProps) {
     return (
         oldProps.groupValues.length === newProps.groupValues.length &&
@@ -64,44 +105,34 @@ function areGroupTablePropsEqual(oldProps, newProps) {
 
 const GroupTable = memo(({groupStateDispatch, groupIndex, groupValues, acumGroupIndex, acumGroupValue}) => {
     // console.log('GroupTable');
+    const [expanded, setExpanded] = React.useState("");
+
+    const handleChange = (panel) => (event, newExpanded) => {
+        setExpanded(newExpanded ? panel : false);
+    };
 
     return (<Box sx={{width: '100%'}}>
         <Paper sx={{width: '100%', mb: 2}} elevation={5}>
-            <TableContainer component={Paper}>
-                <Table aria-label="collapsible table">
-                    <TableHead>
-                        <TableRow>
-                            <StyledTableCell2/>
-                            <StyledTableCell2>
 
-                                <CustomChip
-                                    component="a"
-                                    href="#"
-                                    label="grouping"
-                                />
+            {groupValues
+                .map((groupVal) => (
 
-                            </StyledTableCell2>
-                            <StyledTableCell2/>
-                            <StyledTableCell2 align="right">
+                <Accordion key= {groupVal[0]} expanded={expanded === groupVal[0]} onChange={handleChange(groupVal[0])}>
+                    <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+                        <Row
+                            key={groupVal[0]}
+                            dispatch={groupStateDispatch}
+                            groupValue={groupVal}
+                            groupIndex={groupIndex}
+                            acumGroupIndex={acumGroupIndex}
+                            acumGroupValue={acumGroupValue}
+                            />
+                    </AccordionSummary>
+                    <AccordionDetails>
 
-                                <CustomChip icon={<NumbersIcon/>}></CustomChip>
-                            </StyledTableCell2>
-
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {groupValues
-                            .map((groupVal) => (<CollapsibleRow
-                                key={groupVal[0]}
-                                dispatch={groupStateDispatch}
-                                groupValue={groupVal}
-                                groupIndex={groupIndex}
-                                acumGroupIndex={acumGroupIndex}
-                                acumGroupValue={acumGroupValue}
-                            />))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+                    </AccordionDetails>
+                </Accordion>
+                ))}
         </Paper>
     </Box>);
 }, areGroupTablePropsEqual);
--- fbbotdashboard/src/components/Row.jsx
+++ fbbotdashboard/src/components/Row.jsx
@@ -24,7 +24,7 @@ import TableContainer from "@mui/material/TableContainer";
 import Table from "@mui/material/Table";
 
 
-const StyledGrid = styled(TableCell)(({theme}) => ({
+const StyledCell = styled(TableCell)(({theme}) => ({
     backgroundColor: theme.palette.primary.light, color: theme.palette.common.white, fontSize: 24,
 }));
 
@@ -41,9 +41,7 @@ function areCollapsibleRowPropsEqual(oldProps, newProps) {
 }
 
 
-export const CollapsibleRow = memo(({dispatch, groupValue, groupIndex, acumGroupIndex, acumGroupValue}) => {
-    const [open, setOpen] = React.useState(false);
-
+export const Row = memo(({dispatch, groupValue, groupIndex, acumGroupIndex, acumGroupValue}) => {
     const groupValueName = groupValue[0];
     const groupValueCount = groupValue[1];
 
@@ -84,70 +82,60 @@ export const CollapsibleRow = memo(({dispatch, groupValue, groupIndex, acumGroup
 
     return (<React.Fragment>
 
-        <TableRow sx={{borderBottom: 'unset'}}>
-            <TableCell align="center" colSpan={4}>
-                <Grid container spacing={0}>
+        <TableContainer component={Paper}>
+            <Table sx={{minWidth: 650}} aria-label="simple table">
+                <TableHead>
+                    <TableRow sx={{borderBottom: 'unset'}}>
+                        <StyledCell>
 
-                    <StyledGrid>
-                        <IconButton
-                            aria-label="expand row"
-                            size="small"
-                            onClick={() => setOpen(!open)}
-                        >
-                            {open ? <KeyboardArrowUpIcon/> : <KeyboardArrowDownIcon/>}
-                        </IconButton>
-                    </StyledGrid>
+                            {<Breadcrumbs aria-label="breadcrumb">
+                                {acumGroupIndex.map((groupIndex, i) => (<CustomChip
+                                        key={groupIndex}
+                                        component="a"
+                                        href="#"
+                                        label={acumGroupValue[i]}
+                                        icon={groupIconsByIndex(groupIndex, "text.primary")}
+                                    />
 
-                    <StyledGrid>
+                                ))}
 
-                        {<Breadcrumbs aria-label="breadcrumb">
-                            {acumGroupIndex.map((groupIndex, i) => (<CustomChip
-                                    key={groupIndex}
+                                <CustomChip2
                                     component="a"
                                     href="#"
-                                    label={acumGroupValue[i]}
+                                    label={groupValueName}
                                     icon={groupIconsByIndex(groupIndex, "text.primary")}
                                 />
+                            </Breadcrumbs>}
 
-                            ))}
+                        </StyledCell>
 
-                            <CustomChip2
-                                component="a"
-                                href="#"
-                                label={groupValueName}
-                                icon={groupIconsByIndex(groupIndex, "text.primary")}
-                            />
-                        </Breadcrumbs>}
-
-                    </StyledGrid>
-
-                    <StyledGrid align="right">
-                        <Button onClick={archiveAll} variant="contained" color="secondary">Archive all
-                            group</Button>
-                    </StyledGrid>
+                        <StyledCell align="center">
+                            <Button onClick={archiveAll} variant="contained" color="secondary">Archive all
+                                group</Button>
+                        </StyledCell>
 
 
-                    <StyledGrid align="right">
-                        <Chip label={<Typography variant="h6" gutterBottom>
-                            {groupValueCount}
-                        </Typography>
+                        <StyledCell align="right">
+                            <Chip label={<Typography variant="h6" gutterBottom>
+                                {groupValueCount}
+                            </Typography>
 
-                        } size="medium" color="primary"/>
-                    </StyledGrid>
+                            } size="medium" color="primary"/>
+                        </StyledCell>
 
-                </Grid>
+                    </TableRow>
+                </TableHead>
+            </Table>
 
-            </TableCell>
+        </TableContainer>
 
-        </TableRow>
-
-        <TableRow sx={{borderBottom: 'unset', maxWidth: '100%'}}>
-                <Collapse in={open} timeout="auto" unmountOnExit sx ={{ maxWidth: '500px'}}>
-                    <GroupProvider prevGroupValue={groupValue[0]} prevGroupIndex={groupIndex}>
-                        <GenericCarOrGroupTable/>
-                    </GroupProvider>
-                </Collapse>
-        </TableRow>
+        {/*<TableRow sx={{borderBottom: 'unset', maxWidth: '100%'}}>*/}
+        {/*        <Collapse in={open} timeout="auto" unmountOnExit sx ={{ maxWidth: '500px'}}>*/}
+        {/*            <GroupProvider prevGroupValue={groupValue[0]} prevGroupIndex={groupIndex}>*/}
+        {/*                <GenericCarOrGroupTable/>*/}
+        {/*            </GroupProvider>*/}
+        {/*        </Collapse>*/}
+        {/*</TableRow>*/}
 
         {/*<StyledTableRow>*/}
         {/*    <TableHead>*/}
